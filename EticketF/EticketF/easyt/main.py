55f1a70197767aa5e79d771bf7aed1fe0d14364a
Erdenebayr2 <94421082+Erdenebayr2@users.noreply.github.com>
2023-04-03 00:37:09 +0800

    main

--- EticketF/EticketF/easyt/main.py
+++ EticketF/EticketF/easyt/main.py
@@ -4,8 +4,12 @@ from ticket import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('{user/}', views.user, name='user'),
-    path('{teller/}', views.teller, name='teller'),
-    path('{dashboard/}', views.dashboard, name='dashboard'),
-    path('{contact/}', views.contact, name='contact'),
+    path('user/', views.user, name='user'),
+    path('teller/', views.teller, name='teller'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('contact/', views.contact, name='contact'),
+    path('forget/', views.forget, name='forget'),
+    path('forget2/', views.forget2, name='forget2'),
+    path('ticket/', views.ticket, name='ticket'),
+    path('my_ticket/', views.my_ticket, name='my_ticket'),
 ]
--- EticketF/ticket/views.py
+++ EticketF/ticket/views.py
@@ -61,7 +61,7 @@ def login(request):
             if xp[0] == 1:
                 return redirect('dashboard')
             else:
-                messages.success(request, 'Бүртгэлтэй хэрэглэгч олдсонгүй.')
+                messages.success(request, 'Бүртгэлтэй хэрэглэгч олдсонгүй ! ! !')
                 return redirect('login')
     return render(request,'index.html')
 
@@ -103,3 +103,86 @@ def teller(request):
     
 def contact(request):
     return render(request,'contact.html')
+
+def forget(request):
+    random_string = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz1234567890/$?!', k=4))
+    str = random_string
+    hasho = hashlib.sha256()
+    hasho.update(str.encode('utf8'))
+    hex = hasho.hexdigest()
+    if request.method == 'POST':
+        gmail = request.POST['gmail']
+        con = psycopg2.connect(
+            host='202.131.254.138', 
+            port='5938',
+            database='qrEticket',
+            user='qreasyTicket',
+            password='eba7khulan4'
+            )
+        cur = con.cursor()
+        cur.execute('SELECT count(mail) FROM "user" WHERE mail = %s',[gmail])
+        xp = cur.fetchone()
+        xp = list(xp)
+        print(xp[0])
+        if xp[0] == 1:
+            subject = 'Нууц үг сэргээх'
+            message = 'таны шинэ нууц үг бол '+ hex
+            sender_email = 'eticket123@gmail.com'
+            receiver_email = request.POST.get('gmail')
+            send_mail(
+                subject,
+                message,
+                sender_email,
+                [receiver_email],
+                fail_silently=False,
+            )
+            return redirect('forget2'),HttpResponse({'gmail': gmail})
+        else:
+            messages.error(request, 'Бүртгэлтэй хаяг олдсонгүй ! ! !')
+    return render(request,'forget.html')
+
+def forget2(request):
+    if request.method == 'POST':
+            npass = request.POST['npass']
+            rpass = request.POST['rpass']
+            gmail = request.GET.get('gmail')
+            if npass != rpass:
+                messages.error(request, 'Давталт буруу байна. ! ! !')
+            con = psycopg2.connect(
+                host='202.131.254.138', 
+                port='5938',
+                database='qrEticket',
+                user='qreasyTicket',
+                password='eba7khulan4'
+                )
+            cur = con.cursor()
+            cur.execute('UPDATE "user" SET password = %s WHERE mail = %s',[npass,gmail])
+            con.commit()
+            return redirect('login')
+    return render(request,'forget2.html')
+
+def ticket(request):
+    if request.method == 'POST':
+        loc = request.POST['loc']
+        where = request.POST['where']
+        type = request.POST['type']
+        sex = request.POST['sex']
+        ogno = request.POST['ogno']
+        num = request.POST['num']
+        con = psycopg2.connect(
+                host='202.131.254.138', 
+                port='5938',
+                database='qrEticket',
+                user='qreasyTicket',
+                password='eba7khulan4'
+                )
+        cur = con.cursor()
+        cur.execute("SELECT nextval('mid')")
+        uid = cur.fetchone()[0]
+        cur.execute('INSERT INTO "ticket" (ID,location,where,ttype,thuis,ogno,num) VALUES (%s,%s,%s,%s,%s,%s,%s)',[uid,loc,where,type,sex,ogno,num])
+        con.commit()
+        return redirect('dashboard')
+    return render(request,'ticket.html')
+
+def my_ticket(request):
+    return render(request,'my_ticket.html')
